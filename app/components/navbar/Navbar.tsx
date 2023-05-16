4a896bc634f389f41aabf8201439e93ce239be12
JKKIM <jkesonjk@gmail.com>
2023-05-17 00:42:24 +0900

    #3 feat: Add Container component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,7 +5,19 @@ const Navbar = (props: Props) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="border-b-[1px] py-4">
-        <Container></Container>
+        <Container>
+          <div
+            className="
+            flex
+            items-center
+            justify-between
+            gap-3
+            md:gap-0
+            "
+          >
+            Logo
+          </div>
+        </Container>
       </div>
     </div>
   );
