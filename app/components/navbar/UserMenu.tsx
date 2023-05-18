a0c6d63826f591825e3f3b1fe58def8218f509ae
JKKIM <jkesonjk@gmail.com>
2023-05-18 12:59:50 +0900

    #3 feat: Add UserMenu component for ui

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Avatar from '../Avatar';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-5">
+        <div
+          onClick={() => {}}
+          className="
+            hidden
+            md:block
+            text-sm
+            font-semibold
+            py-3
+            px-4
+            rounded-full
+            hover:bg-neutral-100
+            transition
+            cursor-pointer
+          "
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="
+            p-4
+            md:py-1
+            md:px-2
+            border-[1px]
+            rounded-full
+            border-neutral-200
+            flex
+            items-center
+            cursor-pointer
+            hover:shadow-md
+            transition
+            gap-3
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
