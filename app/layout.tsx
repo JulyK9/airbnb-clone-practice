0ae870aa88606f58591679debb9061a4b674d4dd
JKKIM <jkesonjk@gmail.com>
2023-05-19 16:37:12 +0900

    #5 feat: Add provider for toast in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Nunito } from 'next/font/google';
 import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modal/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
