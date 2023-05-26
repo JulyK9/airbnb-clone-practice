7c4f441adadfbd7770b7e14bc04e5abd0d3fd1c7
JKKIM <jkesonjk@gmail.com>
2023-05-26 11:11:38 +0900

    #9 feat: Add getting current user from db

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-type Props = {};
+import { User } from '@prisma/client';
+
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
 
-const Navbar = (props: Props) => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
+  // console.log({currentUser})
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="border-b-[1px] py-4">
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modal/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
 import LoginModal from './components/modal/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -13,11 +14,13 @@ export const metadata = {
   description: 'Airbnb clone practice',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -25,7 +28,7 @@ export default function RootLayout({
           <ToasterProvider />
           <RegisterModal />
           <LoginModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
