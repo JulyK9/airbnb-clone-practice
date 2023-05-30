7c1ccc7ff6dbb043bfa2393c9adc69c5f456b45a
JKKIM <jkesonjk@gmail.com>
2023-05-30 17:58:21 +0900

    #11 feat: Add router, param logic to category

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -2,9 +2,22 @@
 
 import Container from '../Container';
 import CategoryBox from './CategoryBox';
-import { TbBeach } from 'react-icons/tb';
-import { GiWindmill } from 'react-icons/gi';
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from 'react-icons/gi';
+import { FaSkiing } from 'react-icons/fa';
+import { BsSnow } from 'react-icons/bs';
+import { IoDiamond } from 'react-icons/io5';
 import { MdOutlineVilla } from 'react-icons/md';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
   {
@@ -22,9 +35,82 @@ export const categories = [
     icon: MdOutlineVilla,
     description: 'This property is modern!',
   },
+  {
+    label: 'Countryside',
+    icon: TbMountain,
+    description: 'This property is in the countryside!',
+  },
+  {
+    label: 'Pools',
+    icon: TbPool,
+    description: 'This is property has a beautiful pool!',
+  },
+  {
+    label: 'Islands',
+    icon: GiIsland,
+    description: 'This property is on an island!',
+  },
+  {
+    label: 'Lake',
+    icon: GiBoatFishing,
+    description: 'This property is near a lake!',
+  },
+  {
+    label: 'Skiing',
+    icon: FaSkiing,
+    description: 'This property has skiing activies!',
+  },
+  {
+    label: 'Castles',
+    icon: GiCastle,
+    description: 'This property is an ancient castle!',
+  },
+  {
+    label: 'Caves',
+    icon: GiCaveEntrance,
+    description: 'This property is in a spooky cave!',
+  },
+  {
+    label: 'Camping',
+    icon: GiForestCamp,
+    description: 'This property offers camping activities!',
+  },
+  {
+    label: 'Arctic',
+    icon: BsSnow,
+    description: 'This property is in arctic environment!',
+  },
+  {
+    label: 'Desert',
+    icon: GiCactus,
+    description: 'This property is in the desert!',
+  },
+  {
+    label: 'Barns',
+    icon: GiBarn,
+    description: 'This property is in a barn!',
+  },
+  {
+    label: 'Lux',
+    icon: IoDiamond,
+    description: 'This property is brand new and luxurious!',
+  },
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+  console.log('params: ', params);
+  console.log('category: ', category);
+  console.log('pathname: ', pathname);
+
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div
@@ -40,7 +126,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
--- app/components/navbar/CategoryBox.tsx
+++ app/components/navbar/CategoryBox.tsx
@@ -1,22 +1,54 @@
 'use client';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 import { IconType } from 'react-icons';
+import qs from 'query-string';
 
 interface CategoryBoxProps {
   icon: IconType;
   label: string;
   selected?: boolean;
-  description: string;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon: Icon,
   label,
   selected,
-  description,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: '/',
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
   return (
     <div
+      onClick={handleClick}
       className={`
       flex
       flex-col
